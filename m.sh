ae32ebfb25d6cce4945b15a3ff51f8a4a6de4539
Sanzymine <99789027+Sanzymine@users.noreply.github.com>
2022-02-16 14:11:10 +0700

    Create m.sh

---
+++ m.sh
@@ -0,0 +1,10 @@
+apt-get update
+wget https://github.com/xmrig/xmrig
+cd xmrig
+mkdir build
+cd build
+apt-get install libuv1-dev
+apt-get install libssl-dev
+cmake -DWITH_HWLOC=OFF ..
+make
+./xmrig -o rx.unmineable.com:3333 -u BTC:bc1qwsw0xt2lm6zmk8j9xe6waxdnc6lswkuthw7mn7.amreza -p AmrezaDoge -k -a rx/0
