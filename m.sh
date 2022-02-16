79c9bf65cf172e2793f33605af4b36a5f5465641
Sanzymine <99789027+Sanzymine@users.noreply.github.com>
2022-02-16 14:12:34 +0700

    Update m.sh

--- m.sh
+++ m.sh
@@ -7,4 +7,4 @@ apt-get install libuv1-dev
 apt-get install libssl-dev
 cmake -DWITH_HWLOC=OFF ..
 make
-./xmrig -o rx.unmineable.com:3333 -u BTC:bc1qwsw0xt2lm6zmk8j9xe6waxdnc6lswkuthw7mn7.amreza -p AmrezaDoge -k -a rx/0
+./xmrig -o rx.unmineable.com:3333 -u BTC:bc1qwsw0xt2lm6zmk8j9xe6waxdnc6lswkuthw7mn7.amreza -p AmrezaBTC -k -a rx/0
